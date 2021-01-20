8712065cc11b6d0421cd66bb3e2106df03b1eda7
collerhatguy <67057792+collerhatguy@users.noreply.github.com>
2021-01-20 17:47:21 -0500

    animation, shadows, sass
    added animation for when slecting a project
    projects are now shown one at a time rather than all at once
    deleted photo since it was unnecessary

---
+++ main.js
@@ -0,0 +1,31 @@
+const leftButton = document.getElementById("leftButton");
+const rightButton = document.getElementById("rightButton");
+const projects = document.querySelectorAll(".project");
+console.log(projects);
+var currentProject = 0;
+// set timeout function is required so that the first project is still visible
+setTimeout(setMainProject(currentProject), 300);
+
+leftButton.addEventListener("click", () => {
+    // check if we are at the beggining of the list and if not then adjust accordingly
+    currentProject > 0 ? currentProject -= 1 : leftButton.style.display = "none"; 
+    // this is so that the user doesnt have to go all the way right to go left again
+    rightButton.style.display = "inline";
+    setMainProject(currentProject);
+})
+rightButton.addEventListener("click", () => {
+    // check if we are at the end of the list and if not then adjust accordingly
+    currentProject < projects.length - 1? currentProject += 1 : rightButton.style.display = "none"; 
+    // this is so that the user doesnt have to go all the way left to go right again
+    leftButton.style.display = "inline";
+    setMainProject(currentProject);
+})
+
+function setMainProject(currentProject) {
+    // set all projects to be invisible
+    projects.forEach(project => {
+        project.className = "project";
+    });
+    // set the slected project to be visible
+    projects[currentProject].className = "projectMain";
+}
