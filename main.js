5f569eb5cf32a9f80334e232c724f1b4ab539ac1
collerhatguy <67057792+collerhatguy@users.noreply.github.com>
2021-01-21 11:02:45 -0500

    button adjustment

--- main.js
+++ main.js
@@ -7,15 +7,17 @@ var currentProject = 0;
 setTimeout(setMainProject(currentProject), 300);
 
 leftButton.addEventListener("click", () => {
+    if (currentProject <= 1) leftButton.style.display = "none";
     // check if we are at the beggining of the list and if not then adjust accordingly
-    currentProject > 0 ? currentProject -= 1 : leftButton.style.display = "none"; 
+    if (currentProject > 0) currentProject -= 1; 
     // this is so that the user doesnt have to go all the way right to go left again
     rightButton.style.display = "inline";
     setMainProject(currentProject);
 })
 rightButton.addEventListener("click", () => {
+    if (currentProject === projects.length -2) rightButton.style.display = "none"; 
     // check if we are at the end of the list and if not then adjust accordingly
-    currentProject < projects.length - 1? currentProject += 1 : rightButton.style.display = "none"; 
+    if (currentProject < projects.length - 1) currentProject += 1;
     // this is so that the user doesnt have to go all the way left to go right again
     leftButton.style.display = "inline";
     setMainProject(currentProject);
